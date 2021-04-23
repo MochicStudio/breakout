f34e251430a3dd31c45066f3e73b24efdd6b97a9
Andres Ruiz <andrsruiz@gmail.com>
2021-04-23 00:03:20 -0500

    Set function convention

--- conf.lua
+++ conf.lua
@@ -1,4 +1,4 @@
-function love.conf(t)
+love.conf = function(t)
 	t.identity = nil                    -- The name of the save directory (string)
 	t.appendidentity = false            -- Search files in source directory before save directory (boolean)
 	t.version = "11.3"                  -- The LÖVE version this game was made for (string)
