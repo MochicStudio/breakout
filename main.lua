fd229c4602779a7ea3e26bc32e3651972c58c663
Andres Ruiz <andrsruiz@gmail.com>
2021-04-24 00:58:30 -0500

    Draw Entities

--- main.lua
+++ main.lua
@@ -1,8 +1,13 @@
 local world = require('world')
+local ball = require('entities/ball')
+local paddle = require('entities/paddle')
 
 love.update = function(dt)
 	world:update(dt)
 end
 
 love.draw = function()
+	local ballWX, ballWY = ball.body:getWorldCenter()
+	love.graphics.circle('fill', ballWX, ballWY, ball.shape:getRadius())
+	love.graphics.polygon('fill', paddle.body:getWorldPoints(paddle.shape:getPoints()))
 end
