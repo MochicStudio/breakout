1211e46cf060a11e5291f5c7e4098d1ed1bbbe4e
Andres Ruiz <andrsruiz@gmail.com>
2021-05-06 23:46:20 -0500

    Entity Handles Its Drawing

--- entities/ball.lua
+++ entities/ball.lua
@@ -1,13 +1,23 @@
 -- Ball Entity
 local world = require('world')
 
-local ball = {}
-ball.body = love.physics.newBody(world, 400, 200, 'dynamic')
-ball.body.setMass(ball.body, 32)
-ball.body:setLinearVelocity(300, 300)
-ball.shape = love.physics.newCircleShape(10)
-ball.fixture = love.physics.newFixture(ball.body, ball.shape)
-ball.fixture:setRestitution(1)
-ball.fixture:setUserData(ball)
+return function(posX, posY)
+	local ball = {}
+	ball.body = love.physics.newBody(world, 400, 200, 'dynamic')
+	ball.body.setMass(ball.body, 32)
+	ball.body:setLinearVelocity(300, 300)
+	ball.shape = love.physics.newCircleShape(10)
+	ball.fixture = love.physics.newFixture(ball.body, ball.shape)
+	ball.fixture:setRestitution(1)
+	ball.fixture:setUserData(ball)
 
-return ball
+
+	-- Entity handles it's own draw function
+	ball.draw = function(self)
+		local ballWX, ballWY = ball.body:getWorldCenter()
+
+		love.graphics.circle('fill', ballWX, ballWY, ball.shape:getRadius())
+	end
+
+	return ball
+end
