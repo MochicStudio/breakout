1dfbe97b8a86ab99e5749207235975f23ac9a22f
Andres Ruiz <andrsruiz@gmail.com>
2021-05-07 00:04:17 -0500

    Refactor Boundaries

--- entities/boundary-left.lua
+++ entities/boundary-left.lua
@@ -1,12 +1,19 @@
 -- Boundary Left
 local world = require('world')
-local HALF_HEIGHT = love.graphics.getHeight() / 2
 
-local boundaryLeft = {}
--- We set the x axis as a negative value so we can't see it in the game screen.
-boundaryLeft.body = love.physics.newBody(world, -1, HALF_HEIGHT, 'static')
-boundaryLeft.shape = love.physics.newRectangleShape(1, 600)
-boundaryLeft.fixture = love.physics.newFixture(boundaryLeft.body, boundaryLeft.shape)
-boundaryLeft.fixture:setUserData(boundaryLeft)
+return function(posX, posY)
+	local boundaryLeft = {}
+	-- We set the x axis as a negative value so we can't see it in the game screen.
+	boundaryLeft.body = love.physics.newBody(world, posX, posY, 'static')
+	boundaryLeft.shape = love.physics.newRectangleShape(1, love.graphics.getHeight())
+	boundaryLeft.fixture = love.physics.newFixture(boundaryLeft.body, boundaryLeft.shape)
+	boundaryLeft.fixture:setUserData(boundaryLeft)
 
-return boundaryLeft
+	-- As for boundaries we won't need to write explicitlly to draw
+	-- the entity because is out of the screen game but for code convention
+	-- we still write the draw function.
+	boundaryLeft.draw = function(self)
+	end
+
+	return boundaryLeft
+end
--- entities/boundary-right.lua
+++ entities/boundary-right.lua
@@ -1,14 +1,20 @@
 -- Boundary Rigth
 local world = require('world')
-local MORE_THAN_WIDTH = love.graphics.getWidth() + 1
-local HALF_HEIGHT = love.graphics.getHeight() / 2
 
-local boundaryRight = {}
--- We set the x axis greater than the window's width so we
--- can't see it in the game screen.
-boundaryRight.body = love.physics.newBody(world, MORE_THAN_WIDTH, HALF_HEIGHT, 'static')
-boundaryRight.shape = love.physics.newRectangleShape(1, 600)
-boundaryRight.fixture = love.physics.newFixture(boundaryRight.body, boundaryRight.shape)
-boundaryRight.fixture:setUserData(boundaryRight)
+return function(posX, posY)
+	local boundaryRight = {}
+	-- We set the x axis greater than the window's width so we
+	-- can't see it in the game screen.
+	boundaryRight.body = love.physics.newBody(world, posX, posY, 'static')
+	boundaryRight.shape = love.physics.newRectangleShape(1, love.graphics.getHeight())
+	boundaryRight.fixture = love.physics.newFixture(boundaryRight.body, boundaryRight.shape)
+	boundaryRight.fixture:setUserData(boundaryRight)
 
-return boundaryRight
+	-- As for boundaries we won't need to write explicitlly to draw
+	-- the entity because is out of the screen game but for code convention
+	-- we still write the draw function.
+	boundaryRight.draw = function(self)
+	end
+
+	return boundaryRight
+end
