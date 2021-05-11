58c3890231ba5f0873f68eb3475ee80b3c6caa4e
Andres Ruiz <andrsruiz@gmail.com>
2021-05-10 20:15:37 -0500

    Paddle interacts with ball

--- entities/paddle.lua
+++ entities/paddle.lua
@@ -3,10 +3,18 @@ local world = require('world')
 local input = require('input')
 
 return function(posX, posY)
+	local HALF = 2
+	local windowWidth = love.window.getMode()
+
 	local paddle = {}
+
 	paddle.width = 180
 	paddle.height = 20
-	paddle.body = love.physics.newBody(world, posX, posY, 'static')
+	paddle.speed = 600
+	paddle.leftBoundary = paddle.width / HALF
+	paddle.rightBoundary = windowWidth - (paddle.width / HALF)
+
+	paddle.body = love.physics.newBody(world, posX, posY, 'kinematic')
 	paddle.shape = love.physics.newRectangleShape(paddle.width, paddle.height)
 	paddle.fixture = love.physics.newFixture(paddle.body, paddle.shape)
 	paddle.fixture:setUserData(paddle)
@@ -16,11 +24,7 @@ return function(posX, posY)
 		love.graphics.polygon('fill', paddle.body:getWorldPoints(paddle.shape:getPoints()))
 	end
 
-	paddle.update = function(self, dt)
-		local STEP = 400 * dt -- Works like velocity
-		local HALF = 2
-		local paddleX, paddleY = paddle.body:getPosition()
-
+	paddle.update = function(self)
 		-- Don't move if both keys are been pressed
 		if input.left and input.right then
 			return
@@ -28,10 +32,15 @@ return function(posX, posY)
 
 		-- Move left or right
 		-- We set the paddle boundaries with the paddle width
-		if input.left and paddleX >= paddle.width / HALF then
-			paddle.body:setPosition(paddleX - STEP, paddleY)
-		elseif input.right and paddleX <= (love.graphics.getWidth() - (paddle.width / HALF)) then
-			paddle.body:setPosition(paddleX + STEP, paddleY)
+
+		local selfX = self.body:getX()
+
+		if input.left and selfX >= self.leftBoundary then
+			self.body:setLinearVelocity(-self.speed, 0)
+		elseif input.right and selfX <= self.rightBoundary then
+			self.body:setLinearVelocity(self.speed, 0)
+		else
+			self.body:setLinearVelocity(0, 0)
 		end
 	end
 
