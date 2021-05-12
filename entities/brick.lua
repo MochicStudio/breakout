045cd66eec16101b1cf23911c9411be5ba7d0cae
Andres Ruiz <andrsruiz@gmail.com>
2021-05-11 22:04:43 -0500

    Set Brick Color based on Health

--- entities/brick.lua
+++ entities/brick.lua
@@ -8,7 +8,7 @@ local world = require('world')
 return function(posX, posY)
 	local brick = {}
 
-	brick.health = 2
+	brick.health = 3
 
 	brick.body = love.physics.newBody(world, posX, posY, 'static')
 	brick.shape = love.physics.newRectangleShape(50, 25)
@@ -19,6 +19,18 @@ return function(posX, posY)
 
 	-- Entity handles it's own draw function
 	brick.draw = function(self)
+		local HEALTHY = 3
+		local HALF_WAY = 2
+		local ALMOST_DEAD = 1
+		-- Set color based on health
+		if self.health == HEALTHY then
+			love.graphics.setColor(0, 1, 0, 1)
+		elseif self.health == HALF_WAY then
+			love.graphics.setColor(1, 1, 0, 1)
+		elseif self.health == ALMOST_DEAD then
+			love.graphics.setColor(1, 0, 0, 1)
+		end
+
 		love.graphics.polygon('fill', brick.body:getWorldPoints(brick.shape:getPoints()))
 	end
 
