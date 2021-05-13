7cd864a21764a81349b4eeefd1a4a9f1cb7665f3
Andres Ruiz <andrsruiz@gmail.com>
2021-05-12 19:56:45 -0500

    Draw Brick with a sequence

--- entities.lua
+++ entities.lua
@@ -8,14 +8,31 @@ local paddle = require('entities/paddle')
 local HALF = 2
 local THREE_QUARTER = 0.75
 
-return {
+local entities = {
 	boundaryTop(love.graphics.getWidth() / HALF, -1),
 	boundaryVertical(love.graphics.getWidth() + 1, love.graphics.getHeight() / HALF), -- Right
 	boundaryBottom(love.graphics.getWidth() / HALF, love.graphics.getHeight() + 1),
 	boundaryVertical(-1, love.graphics.getHeight() / HALF), -- Left
 	ball(love.graphics.getWidth() / HALF, love.graphics.getHeight() / HALF),
 	paddle(love.graphics.getWidth() / HALF, love.graphics.getHeight() * THREE_QUARTER),
-	brick(50, 50),
-	brick(101, 50),
-	brick(152, 50)
 }
+
+local DOUBLE = 2
+local BRICK_WIDTH = 50
+local BRICK_HEIGHT = 20
+local SPACE_X = 50
+local SPACE_Y = 25
+local columns = ((love.graphics.getWidth() - (SPACE_X * DOUBLE)) / BRICK_WIDTH) - 2
+local rows = 5
+
+print(love.graphics.getWidth() - SPACE_X)
+print(columns)
+for x = 1, math.abs(columns) do
+	for y = 1, rows do
+		local brickX = BRICK_WIDTH + (SPACE_X + 5) * x
+		local brickY = BRICK_HEIGHT + SPACE_Y * y
+		entities[#entities + 1] = brick(brickX, brickY)
+	end
+end
+
+return entities
