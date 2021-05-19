844e859090110cbb6888a57e36211048e3041276
Andres Ruiz <andrsruiz@gmail.com>
2021-05-18 23:34:34 -0500

    Add Begin Contact Callback

--- world.lua
+++ world.lua
@@ -2,6 +2,13 @@
 
 -- Collision's callbacks
 local beginContact = function(fixtureA, fixtureB, contact)
+	-- Get entities that are being in contact
+	local entityA = fixtureA:getUserData()
+	local entityB = fixtureB:getUserData()
+
+	-- If entity has endContact mehtod, execute it
+	if entityA.beginContact then entityA:beginContact() end
+	if entityB.beginContact then entityB:beginContact() end
 end
 
 local endContact = function(fixtureA, fixtureB, contact)
