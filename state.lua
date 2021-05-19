c71933035d4cb7af0419a185e90ae77e4bc85118
Andres Ruiz <andrsruiz@gmail.com>
2021-05-18 22:22:30 -0500

    State in own file

---
+++ state.lua
@@ -0,0 +1,14 @@
+-- State of the game. This way our data is separated from our functionality
+return {
+	buttonLeft = false,
+	buttonRight = false,
+	gameOver = false,
+	palette = {
+		{0, 1, 0, 1}, -- green
+		{1, 1, 0, 1}, -- yellow
+		{1, 0, 0, 1}, -- red
+		{1, 1, 1, 1} -- white
+	},
+	paused = false,
+	stageCleared = false
+}
